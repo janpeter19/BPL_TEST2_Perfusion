2192f3d66a45ed6409a0b91faa77b9f4d783f299
Jan Peter Axelsson <jan.peter.axelsson@icloud.com>
2023-03-28 16:52:48 +0200

    Update BPL_TEST2_Perfusion_fmpy_explore.py
    Just changed NCP to ncp for compliance with PyFMI notation

--- BPL_TEST2_Perfusion_fmpy_explore.py
+++ BPL_TEST2_Perfusion_fmpy_explore.py
@@ -66,9 +66,9 @@ elif platform.system() == 'Linux':
 
 # Provide various opts-profiles
 if flag_type in ['CS', 'cs']:
-   opts_std = {'NCP': 500}
+   opts_std = {'ncp': 500}
 elif flag_type in ['ME', 'me']:
-   opts_std = {'NCP': 500}
+   opts_std = {'ncp': 500}
 else:    
    print('There is no FMU for this platform')
   
@@ -560,7 +560,7 @@ def simu(simulationTime=simulationTime, mode='Initial', options=opts_std, diagra
          validate = False,
          start_time = 0,
          stop_time = simulationTime,
-         output_interval = simulationTime/options['NCP'],
+         output_interval = simulationTime/options['ncp'],
          record_events = True,
          start_values = start_values,
          fmi_call_logger = None,
@@ -596,7 +596,7 @@ def simu(simulationTime=simulationTime, mode='Initial', options=opts_std, diagra
             validate = False,
             start_time = prevFinalTime,
             stop_time = prevFinalTime + simulationTime,
-            output_interval = simulationTime/options['NCP'],
+            output_interval = simulationTime/options['ncp'],
             record_events = True,
             start_values = start_values,
             fmi_call_logger = None,
