42e5d3c6b7de2dcf068e0fd96cad41ccca3d580b
Jan Peter Axelsson <jan.peter.axelsson@icloud.com>
2025-06-12 10:28:01 +0200

    Test new Linux FMU with MSL 4.1.0

--- BPL_TEST2_Perfusion_explore.py
+++ BPL_TEST2_Perfusion_explore.py
@@ -60,6 +60,7 @@
 # 2024-08-31 - Adjusted the print text for Open Modelica 
 # 2024-10-24 - Update BPL 2.2.2 - GUI
 # 2024-11-07 - Update BPL 2.3.0
+# 2025-06-12 - Test MSL 4.1.0 with OpenModelica genreated FMU
 #------------------------------------------------------------------------------------------------------------------
 
 # Setup framework
@@ -129,8 +130,8 @@ if flag_vendor in ['JM', 'jm']:
    MSL_version = model.get('MSL.version')[0]
    BPL_version = model.get('BPL.version')[0]
 elif flag_vendor in ['OM', 'om']:
-   MSL_usage = '3.2.3 - used components: RealInput, RealOutput, CombiTimeTable, Types' 
-   MSL_version = '3.2.3'
+   MSL_usage = '4.1.0 - used components: RealInput, RealOutput, CombiTimeTable, Types' 
+   MSL_version = '4.1.0'
    BPL_version = 'Bioprocess Library version 2.3.0' 
 else:    
    print('There is no FMU for this platform')
